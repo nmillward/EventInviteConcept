5db86d037d860637ea84e1ea65395e558d8e0e08
Nick Millward <nicholasmillward@gmail.com>
2016-05-21 18:57:12 -0400

    add fab to layout

--- app/src/main/java/com/nickmillward/eventinviteconcept/EventDetailsActivity.java
+++ app/src/main/java/com/nickmillward/eventinviteconcept/EventDetailsActivity.java
@@ -1,7 +1,9 @@
 package com.nickmillward.eventinviteconcept;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class EventDetailsActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class EventDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details_event);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.event_detail_toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.event_detail_fab);
+
     }
 }
