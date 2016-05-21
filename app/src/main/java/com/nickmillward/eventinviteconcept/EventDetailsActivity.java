6381749897662c98400a73e1c256bd046958c7a2
Nick Millward <nicholasmillward@gmail.com>
2016-05-21 18:21:39 -0400

    initial commit

---
+++ app/src/main/java/com/nickmillward/eventinviteconcept/EventDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.nickmillward.eventinviteconcept;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EventDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_event);
+    }
+}
