0e6ad8be17d001a6a4c5b516e93d2a55e904e438
Nick Millward <nicholasmillward@gmail.com>
2016-05-24 08:50:53 -0400

    cleanup xml

--- app/src/main/java/com/nickmillward/eventinviteconcept/EventDetailsActivity.java
+++ app/src/main/java/com/nickmillward/eventinviteconcept/EventDetailsActivity.java
@@ -31,10 +31,7 @@ public class EventDetailsActivity extends AppCompatActivity implements View.OnCl
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.event_detail_toolbar);
         setSupportActionBar(toolbar);
-
-//        fabIcon = (ImageView) findViewById(R.id.event_detail_fab_icon);
-//        fabIcon.setRotation(45);    //Set cross default rotation to 45 --> plus sign
-
+        
         fab = (FloatingActionButton) findViewById(R.id.event_detail_fab);
         isFabBgVisible = true;
 
