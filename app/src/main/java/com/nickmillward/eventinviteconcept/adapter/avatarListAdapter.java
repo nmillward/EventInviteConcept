bbddee2a089489054eb3dcf270ce830e0082860d
Nick Millward <nicholasmillward@gmail.com>
2016-06-10 15:33:47 -0400

    update listAdapter

--- app/src/main/java/com/nickmillward/eventinviteconcept/adapter/avatarListAdapter.java
+++ app/src/main/java/com/nickmillward/eventinviteconcept/adapter/avatarListAdapter.java
@@ -1,21 +1,37 @@
 package com.nickmillward.eventinviteconcept.adapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.nickmillward.eventinviteconcept.R;
+import com.nickmillward.eventinviteconcept.util.ImageLoader;
 
 /**
  * Created by nmillward on 6/10/16.
  */
 public class AvatarListAdapter extends RecyclerView.Adapter<AvatarListAdapter.AvatarViewHolder> {
 
+    private ImageLoader imageLoader;
+
+    private LayoutInflater inflater;
+
     @Override
     public AvatarViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = inflater.inflate(R.layout.item_avatar_invite, parent, false);
+
+        AvatarViewHolder holder = new AvatarViewHolder(view);
+
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(final AvatarViewHolder holder, int position) {
+
+
 
     }
 
@@ -26,8 +42,14 @@ public class AvatarListAdapter extends RecyclerView.Adapter<AvatarListAdapter.Av
 
     class AvatarViewHolder extends RecyclerView.ViewHolder {
 
+        ImageView avatarImage;
+        TextView avatarName;
+
         public AvatarViewHolder(View itemView) {
             super(itemView);
+
+            avatarImage = (ImageView) itemView.findViewById(R.id.iv_avatar_item);
+            avatarName = (TextView) itemView.findViewById(R.id.tv_avatar_item);
         }
     }
 }
