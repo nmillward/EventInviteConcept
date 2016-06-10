ba488dc841404c4e8ea044c08201f5e8e54d7b2d
Nick Millward <nicholasmillward@gmail.com>
2016-06-09 21:12:29 -0400

    add picasso lib, create circleTransform util

---
+++ app/src/main/java/com/nickmillward/eventinviteconcept/Util/ImageLoader.java
@@ -0,0 +1,15 @@
+package com.nickmillward.eventinviteconcept.Util;
+
+import android.widget.ImageView;
+
+import com.squareup.picasso.Transformation;
+
+/**
+ * Created by nmillward on 6/9/16.
+ */
+public interface ImageLoader {
+
+    void load(ImageView view, String url);
+
+    void loadWithTransformation(ImageView view, String url, Transformation transformation);
+}
---
+++ app/src/main/java/com/nickmillward/eventinviteconcept/Util/PicassoImageLoader.java
@@ -0,0 +1,29 @@
+package com.nickmillward.eventinviteconcept.Util;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
+
+/**
+ * Created by nmillward on 6/9/16.
+ */
+public class PicassoImageLoader implements ImageLoader {
+
+    private Picasso picasso;
+
+    public PicassoImageLoader(Context context) {
+        this.picasso = Picasso.with(context);
+    }
+
+    @Override
+    public void load(ImageView view, String url) {
+        picasso.load(url).into(view);
+    }
+
+    @Override
+    public void loadWithTransformation(ImageView view, String url, Transformation transformation) {
+        picasso.load(url).transform(transformation).into(view);
+    }
+}
