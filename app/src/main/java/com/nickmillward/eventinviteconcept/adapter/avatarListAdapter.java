ff0b1c43ca36f5c55981853e0c24c2423f74e4f6
Nick Millward <nicholasmillward@gmail.com>
2016-06-12 21:40:41 -0400

    add recyclerview animation

--- app/src/main/java/com/nickmillward/eventinviteconcept/adapter/avatarListAdapter.java
+++ app/src/main/java/com/nickmillward/eventinviteconcept/adapter/avatarListAdapter.java
@@ -4,6 +4,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -38,6 +41,10 @@ public class AvatarListAdapter extends RecyclerView.Adapter<AvatarListAdapter.Av
         final Avatar avatar = avatars.get(position);
         imageLoader.loadWithTransformation(holder.avatarImage, avatar.getAvatarImage(), new CircleTransform());
         holder.avatarName.setText(avatar.getAvatarName());
+
+        //Set view to fade in
+        setFadeAnimation(holder.itemView);
+//        setScaleAnimation(holder.itemView);
     }
 
     @Override
@@ -45,7 +52,7 @@ public class AvatarListAdapter extends RecyclerView.Adapter<AvatarListAdapter.Av
         return avatars.size();
     }
 
-    public class AvatarViewHolder extends RecyclerView.ViewHolder {
+    public class AvatarViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         ImageView avatarImage;
         TextView avatarName;
@@ -56,5 +63,22 @@ public class AvatarListAdapter extends RecyclerView.Adapter<AvatarListAdapter.Av
             avatarImage = (ImageView) itemView.findViewById(R.id.iv_avatar_item);
             avatarName = (TextView) itemView.findViewById(R.id.tv_avatar_item);
         }
+
+        @Override
+        public void onClick(View view) {
+
+        }
+    }
+
+    private void setFadeAnimation(View view) {
+        AlphaAnimation anim = new AlphaAnimation(0.0f, 1.0f);
+        anim.setDuration(1000);
+        view.startAnimation(anim);
+    }
+
+    private void setScaleAnimation(View view) {
+        ScaleAnimation anim = new ScaleAnimation(0.0f, 1.0f, 0.0f, 1.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        anim.setDuration(300);
+        view.startAnimation(anim);
     }
 }
