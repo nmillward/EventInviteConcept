3525373d89397193be24ec3d7c405a0aab2f0a1c
Nick Millward <nicholasmillward@gmail.com>
2016-06-27 17:11:50 -0400

    create and setup invite dialog activity

---
+++ app/src/main/java/com/nickmillward/eventinviteconcept/ui/InviteConfirmActivity.java
@@ -0,0 +1,16 @@
+package com.nickmillward.eventinviteconcept.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.nickmillward.eventinviteconcept.R;
+
+public class InviteConfirmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invite_confirm);
+    }
+
+}
