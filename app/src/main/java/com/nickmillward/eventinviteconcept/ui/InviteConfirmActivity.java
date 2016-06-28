0239f29672b47d4f5f22a08921563570862010b3
Nick Millward <nicholasmillward@gmail.com>
2016-06-27 21:30:51 -0400

    setup intents for dialog activity

--- app/src/main/java/com/nickmillward/eventinviteconcept/ui/InviteConfirmActivity.java
+++ app/src/main/java/com/nickmillward/eventinviteconcept/ui/InviteConfirmActivity.java
@@ -1,16 +1,32 @@
 package com.nickmillward.eventinviteconcept.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.nickmillward.eventinviteconcept.R;
 
 public class InviteConfirmActivity extends AppCompatActivity {
 
+    private Button confirmButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_invite_confirm);
+
+        confirmButton = (Button) findViewById(R.id.btn_confirm);
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(InviteConfirmActivity.this, EventDetailsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 
 }
