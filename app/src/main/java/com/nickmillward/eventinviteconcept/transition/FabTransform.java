e825598a07fcfa409c7b178e2822ecca925e95e5
Nick Millward <nicholasmillward@gmail.com>
2016-06-27 21:52:53 -0400

    start fab tranform setup

---
+++ app/src/main/java/com/nickmillward/eventinviteconcept/transition/FabTransform.java
@@ -0,0 +1,21 @@
+package com.nickmillward.eventinviteconcept.transition;
+
+import android.transition.Transition;
+import android.transition.TransitionValues;
+
+/**
+ * Created by nmillward on 6/27/16.
+ */
+public class FabTransform extends Transition {
+
+
+    @Override
+    public void captureStartValues(TransitionValues transitionValues) {
+
+    }
+
+    @Override
+    public void captureEndValues(TransitionValues transitionValues) {
+
+    }
+}
