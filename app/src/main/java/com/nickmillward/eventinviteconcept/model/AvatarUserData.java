5a5a24552a94a985cfaba8f8b2fc9758df8b98c1
Nick Millward <nicholasmillward@gmail.com>
2016-06-28 17:19:30 -0400

    update mock avatar data

--- app/src/main/java/com/nickmillward/eventinviteconcept/model/AvatarUserData.java
+++ app/src/main/java/com/nickmillward/eventinviteconcept/model/AvatarUserData.java
@@ -17,14 +17,14 @@ public class AvatarUserData {
         avatars = new ArrayList<>();
 
         // Mock Data
+        avatars.add(new Avatar("http://pbs.twimg.com/profile_images/603269306026106880/42CwEF4n.jpg", "Tom"));
+        avatars.add(new Avatar("http://pbs.twimg.com/profile_images/651445595081605121/0B67DmRf.png", "Oprah"));
+        avatars.add(new Avatar("http://pbs.twimg.com/profile_images/668328458519384064/FSAIjKRl.jpg", "Jack"));
+        avatars.add(new Avatar("http://pbs.twimg.com/profile_images/585565077207678977/N_eNSBXi.jpg", "Kanye"));
+        avatars.add(new Avatar("http://pbs.twimg.com/profile_images/668902554957316096/IpjBGyjC.jpg", "Chris"));
+        avatars.add(new Avatar("http://pbs.twimg.com/profile_images/738744285101580288/OUoCVEXG.jpg", "Barack"));
         avatars.add(new Avatar("http://pbs.twimg.com/profile_images/710185851498729473/80w95ZyZ.jpg", "Nick"));
-        avatars.add(new Avatar("http://pbs.twimg.com/profile_images/710185851498729473/80w95ZyZ.jpg", "Nick"));
-        avatars.add(new Avatar("http://pbs.twimg.com/profile_images/710185851498729473/80w95ZyZ.jpg", "Nick"));
-        avatars.add(new Avatar("http://pbs.twimg.com/profile_images/710185851498729473/80w95ZyZ.jpg", "Nick"));
-        avatars.add(new Avatar("http://pbs.twimg.com/profile_images/710185851498729473/80w95ZyZ.jpg", "Nick"));
-        avatars.add(new Avatar("http://pbs.twimg.com/profile_images/710185851498729473/80w95ZyZ.jpg", "Nick"));
-        avatars.add(new Avatar("http://pbs.twimg.com/profile_images/710185851498729473/80w95ZyZ.jpg", "Nick"));
-        avatars.add(new Avatar("http://pbs.twimg.com/profile_images/710185851498729473/80w95ZyZ.jpg", "Nick"));
+        avatars.add(new Avatar("http://pbs.twimg.com/profile_images/1269252993/HeidiKlum.jpg", "Heidi"));
 
         return avatars;
     }
